cc63a2bda169163fc212863ba8fd4274c0f6131d
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2023-12-07 16:56:39 +0700

    Add square_bracket.py

---
+++ chapter_02/examples/square_bracket.py
@@ -0,0 +1,9 @@
+saying = "Don't panic!"
+letters = list(saying)
+print("letters:", letters)
+print("letters[0]:", letters[0])   # D
+print("letters[3]:", letters[3])   # '
+print("letters[6]:", letters[6])   # p
+print("letters[-1]:", letters[-1]) # !
+print("letters[-3]:", letters[-3]) # i
+print("letters[-6]:", letters[-6]) # p
